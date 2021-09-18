840bc2c6089886269fee8ea68be591d6f63b23a4
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-09-18 23:27:49 +0000

    Add profile model

--- src/app/models.py
+++ src/app/models.py
@@ -1,1 +1,15 @@
-# Define models below
+from sqlmodel import Field, SQLModel
+
+
+class ProfileBase(SQLModel):
+    first_name: str
+    last_name: str
+    surname: str
+
+
+class Profile(ProfileBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+
+
+class ProfileCreate(ProfileBase):
+    pass
