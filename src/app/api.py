e2d72badd811bf38b3b4a91add271f06d9691a7a
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-09-18 23:31:12 +0000

    Add people routes

--- src/app/api.py
+++ src/app/api.py
@@ -1,14 +1,42 @@
 from fastapi import Depends, FastAPI
 from fastapi_health import health
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 from app.db import get_session
+from app.models import Profile, ProfileCreate
 
 api = FastAPI()
 
 
-def is_database_online(session: AsyncSession = Depends(get_session)):
-    return session
+@api.get("/healthz")
+async def healthz(session: AsyncSession = Depends(get_session)):
+    return health([session])
 
 
-api.add_api_route("/healthz", health([is_database_online]))
+@api.get("/people", response_model=list[Profile])
+async def get_people(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Profile))
+    people = result.scalars().all()
+    return [
+        Profile(
+            first_name=person.first_name,
+            last_name=person.last_name,
+            surname=person.surname,
+            id=person.id,
+        )
+        for person in people
+    ]
+
+
+@api.post("/people")
+async def add_person(
+    person: ProfileCreate, session: AsyncSession = Depends(get_session)
+):
+    profile = Profile(
+        first_name=person.first_name, last_name=person.last_name, surname=person.surname
+    )
+    session.add(profile)
+    await session.commit()
+    await session.refresh(profile)
+    return profile
