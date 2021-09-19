d3a403835220cf0e1ce77c16de6c62bef8442c6d
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-09-19 00:08:57 +0000

    Update tests

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,6 +1,19 @@
-def test_api_health(client):
+from app.models import ProfileCreate
+
+
+def test_get_people(client):
     response = client.get(
-        "/healthz/",
+        "/people",
     )
     assert response.status_code == 200
-    assert response.json() == {}
+    # assert response.json() == []
+
+
+def test_post_people(client):
+    person = ProfileCreate(first_name="first", last_name="last", surname="surname")
+    response = client.post("/people", json=person.dict())
+    assert response.status_code == 200
+    assert "id" in response.json()
+    assert response.json()["first_name"] == person.first_name
+    assert response.json()["last_name"] == person.last_name
+    assert response.json()["surname"] == person.surname
