2031f3c182af5f1a390ccacb19ebd3480489c077
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-09-20 19:05:02 +0000

    Add pagination
    - Closes #6

--- src/app/api.py
+++ src/app/api.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi_pagination import LimitOffsetPage, add_pagination, paginate
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
@@ -8,25 +9,33 @@ from app.models import Profile, ProfileCreate
 api = FastAPI()
 
 
-@api.get("/people", response_model=list[Profile])
+@api.get("/people", response_model=LimitOffsetPage[Profile])
 async def get_people(session: AsyncSession = Depends(get_session)):
+    """
+    Return all people
+    """
     result = await session.execute(select(Profile))
     people = result.scalars().all()
-    return [
-        Profile(
-            first_name=person.first_name,
-            last_name=person.last_name,
-            surname=person.surname,
-            id=person.id,
-        )
-        for person in people
-    ]
+    return paginate(
+        [
+            Profile(
+                first_name=person.first_name,
+                last_name=person.last_name,
+                surname=person.surname,
+                id=person.id,
+            )
+            for person in people
+        ]
+    )
 
 
 @api.post("/people")
 async def add_person(
     person: ProfileCreate, session: AsyncSession = Depends(get_session)
 ):
+    """
+    Add a person
+    """
     profile = Profile(
         first_name=person.first_name, last_name=person.last_name, surname=person.surname
     )
@@ -34,3 +43,6 @@ async def add_person(
     await session.commit()
     await session.refresh(profile)
     return profile
+
+
+add_pagination(api)
