07fe235e45b6339e662ea80d030aaa66a2c5a93e
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-09-21 21:44:47 +0000

    Update tests

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,25 +1,26 @@
-from app.models import ProfileCreate
+from app.schemas import ProfileDetails
 
 
 def test_get_people(client):
     response = client.get(
-        "/people",
+        "/profile",
     )
     assert response.status_code == 200
 
 
-def test_post_people(client):
-    person = ProfileCreate(first_name="first", last_name="last", surname="surname")
-    response = client.post("/people", json=person.dict())
+def test_create_profile(client):
+    profile = ProfileDetails(name="first last", bio={"about": "tester", "other": "aob"})
+    response = client.post("/profile", json=profile.dict())
     assert response.status_code == 200
     assert "id" in response.json()
-    assert response.json()["first_name"] == person.first_name
-    assert response.json()["last_name"] == person.last_name
+    assert "active" in response.json()
+    assert response.json()["name"] == profile.name
+    assert response.json()["bio"] == profile.bio
 
 
-def test_put_people():
+def test_update_profile():
     pass
 
 
-def test_delete_people():
+def test_delete_profile():
     pass
