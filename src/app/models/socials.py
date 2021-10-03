577c8d6fb51c4d486273761d6bd224d5d2f7b64f
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-10-03 14:18:07 +0000

    Update routes

--- src/app/models/socials.py
+++ src/app/models/socials.py
@@ -6,23 +6,6 @@ from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_
 
 from app.models.mixins import TimestampMixin
 
-# from app.utils import get_account_url
-
-
-class SocialNetwork(TimestampMixin, models.Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=20, unique=True)
-    domain = fields.CharField(max_length=50, null=True)
-    account_prefix = fields.CharField(max_length=30, null=True)
-
-
-SocialNetworkModel = pydantic_model_creator(SocialNetwork, name="SocialNetworkModel")
-
-
-SocialNetworkCreateModel = pydantic_model_creator(
-    SocialNetwork, name="SocialNetworkCreateModel", exclude_readonly=True
-)
-
 
 class SocialAccount(TimestampMixin, models.Model):
     id = fields.IntField(pk=True)
@@ -30,9 +13,15 @@ class SocialAccount(TimestampMixin, models.Model):
     network = fields.ForeignKeyField("models.SocialNetwork", related_name="accounts")
     user = fields.ForeignKeyField("models.User", related_name="social_accounts")
 
-    # @property
-    # async def url(self) -> str:
-    #     return get_account_url(self.network, self)
+    @property
+    async def url(self) -> str:
+        from app.utils import get_account_url  # noqa
+
+        social_network = await self.network
+        return get_account_url(social_network, self)
+
+    async def __str__(self):
+        return await self.url
 
     class Meta:
         unique_together = (("username", "network"),)
@@ -46,3 +35,21 @@ SocialAccountListModel = pydantic_queryset_creator(SocialAccount)
 class SocialAccountCreateModel(BaseModel):
     username: str
     user_id: UUID
+
+
+class SocialNetwork(TimestampMixin, models.Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=20, unique=True)
+    domain = fields.CharField(max_length=50, null=True)
+    account_prefix = fields.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return f"{self.name}: https://{self.domain}"
+
+
+SocialNetworkModel = pydantic_model_creator(SocialNetwork, name="SocialNetworkModel")
+
+
+SocialNetworkCreateModel = pydantic_model_creator(
+    SocialNetwork, name="SocialNetworkCreateModel", exclude_readonly=True
+)
