0e0fee8456afa4c41c6fba70153663521039def8
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-10-10 22:35:20 +0000

    Add api tests

--- src/tests/api/test_api.py
+++ src/tests/api/test_api.py
@@ -1,5 +1,37 @@
+from jsf import JSF
+
+from app.models.users import UserCreate
+
+
 def test_docs(client):
     response = client.get(
         "/docs",
     )
     assert response.status_code == 200
+
+
+def test_register_user(client):
+    user = JSF(UserCreate.schema()).generate()
+    response = client.post("/auth/register", json=user)
+    assert response.ok
+    assert response.status_code == 201
+    assert not response.json()["is_verified"]
+    assert response.json()["name"] == user["name"]
+    assert response.json()["email"] == user["email"]
+
+
+def test_login_jwt(client):
+    user = JSF(UserCreate.schema()).generate()
+    client.post(
+        "/auth/register",
+        json=user,
+    )
+    login_data = {"username": user["email"], "password": user["password"]}
+    response = client.post(
+        "/auth/jwt/login",
+        data=login_data,
+    )
+    assert response.ok
+    assert response.status_code == 200
+    assert response.json()["access_token"]
+    assert response.json()["token_type"] == "bearer"
--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,15 +1,18 @@
 from typing import Generator
 
 import pytest
+from decouple import config
 from fastapi.testclient import TestClient
 from tortoise.contrib.test import finalizer, initializer
 
 from app.main import api
 
+TEST_DB_URL = config("TEST_DB_URL", default="sqlite://:memory:")
+
 
 @pytest.fixture(scope="module")
 def client() -> Generator:
-    initializer(["app.models"])
+    initializer(modules=["app.models"], db_url=TEST_DB_URL)
     with TestClient(api) as c:
         yield c
     finalizer()
