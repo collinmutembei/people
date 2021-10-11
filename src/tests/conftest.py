d307f32071a7925f89dca58e7cc7260b6801db88
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-10-11 15:28:00 +0000

    Configure TORTOISE_TEST_DB

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,20 +1,28 @@
 from typing import Generator
 
 import pytest
+from decouple import config
 from fastapi.testclient import TestClient
 from tortoise.contrib.test import finalizer, initializer
 
 from app.main import api
 
 
+@pytest.fixture(scope="session", autouse=True)
+def initialize_tests(request):
+    initializer(
+        modules=["app.models"],
+        db_url=config("TORTOISE_TEST_DB", default="sqlite://:memory:"),
+    )
+    request.addfinalizer(finalizer)
+
+
 @pytest.fixture(scope="module")
 def client() -> Generator:
-    initializer(modules=["app.models"])
     with TestClient(api) as c:
         yield c
-    finalizer()
 
 
 @pytest.fixture(scope="module")
-def event_loop(client: TestClient) -> Generator:
-    yield client.task.get_loop()
+def event_loop(app_client: TestClient) -> Generator:
+    yield app_client.task.get_loop()
