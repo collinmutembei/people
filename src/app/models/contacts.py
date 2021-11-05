9e2bbfa265933b48af8daf4c2313a2f72817c455
Collin Mutembei <10899666+collinmutembei@users.noreply.github.com>
2021-11-05 18:23:49 +0000

    Add ContactsFile model

---
+++ src/app/models/contacts.py
@@ -0,0 +1,11 @@
+from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+class ContactsFile(models.Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=50)
+    uploaded_at = fields.DatetimeField(auto_now_add=True)
+    user = fields.ForeignKeyField("models.User", related_name="contacts_files")
+
+
+ContactsFileSchema = pydantic_model_creator(ContactsFile, name="ContactsFile")
