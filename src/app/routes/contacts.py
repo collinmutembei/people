e064d595099cdd188fcdc7b9653bbb8b4b09f28b
Collin Mutembei <10899666+collinmutembei@users.noreply.github.com>
2021-11-05 18:25:01 +0000

    Add upload_contacts route

---
+++ src/app/routes/contacts.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends, UploadFile, File
+
+from app.core.users import fastapi_users
+from app.models.contacts import ContactsFile, ContactsFileSchema
+
+router = APIRouter()
+
+
+@router.post("/upload")
+async def upload_contacts(
+    contacts: UploadFile = File(...), user=Depends(fastapi_users.current_user(active=True))
+):
+    #  TODO: Read CSV file and create users and social accounts
+    contact_file_obj = await ContactsFile.create(name=contacts.filename, user=user)
+    return await ContactsFileSchema.from_tortoise_orm(contact_file_obj)
