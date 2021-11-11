af310cb8c78a5424af53d228a8c1fa8da529f8ca
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-11-11 18:26:56 +0000

    Update file name max_length

--- src/app/models/contacts.py
+++ src/app/models/contacts.py
@@ -4,7 +4,7 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 
 class ContactsFile(models.Model):
     id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=50)
+    name = fields.CharField(max_length=100)
     uploaded_at = fields.DatetimeField(auto_now_add=True)
     uploader = fields.ForeignKeyField("models.User", related_name="contacts_files")
 
--- src/migrations/models/1_20211111182531_initial.sql
+++ src/migrations/models/1_20211111182531_initial.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS "socialaccount" (
 );
 CREATE TABLE IF NOT EXISTS "contactsfile" (
     "id" SERIAL NOT NULL PRIMARY KEY,
-    "name" VARCHAR(50) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
     "uploaded_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
     "uploader_id" UUID NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE
 );
