895891e2786f3a571a08f48de4f85e442c691610
Collin Mutembei <10899666+collinmutembei@users.noreply.github.com>
2024-03-19 08:51:09 +0000

    Add UI components

--- src/api/main.py
+++ src/api/main.py
@@ -3,6 +3,12 @@ from contextlib import asynccontextmanager
 
 from beanie import init_beanie
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastui import AnyComponent, FastUI
+from fastui import components as c
+from fastui import prebuilt_html
+from fastui.components.display import DisplayLookup, DisplayMode
+from fastui.events import BackEvent, GoToEvent
 from loguru import logger
 
 from api.core.users import (  # current_active_user,
@@ -92,12 +98,70 @@ api.include_router(
     tags=["users"],
 )
 
-
-# @api.get("/authenticated-route")
-# async def authenticated_route(user: User = Depends(current_active_user)):
-#     return {"message": f"Hello {user.email}!"}
-
-
 api.include_router(SocialNetworkRouter, prefix="/socialnetwork", tags=["socialnetwork"])
 api.include_router(SocialProfileRouter, prefix="/socialprofile", tags=["socialprofile"])
 api.include_router(ContactsUploadRouter, prefix="/contacts", tags=["uploads"])
+
+
+@api.get("/api/", response_model=FastUI, response_model_exclude_none=True)
+async def networks_table() -> list[AnyComponent]:
+    """
+    Show a table of people, `/api` is the endpoint the frontend will connect to
+    when a user visits `/` to fetch components to render.
+    """
+    networks = await SocialNetwork.find_all(fetch_links=True).to_list()
+    return [
+        c.Page(
+            components=[
+                c.Heading(text="People", level=2),
+                c.Table(
+                    data=networks,
+                    columns=[
+                        DisplayLookup(
+                            field="name",
+                            on_click=GoToEvent(url="/network/{name}/"),
+                        ),
+                        DisplayLookup(
+                            field="domain",
+                        ),
+                    ],
+                ),
+            ]
+        ),
+    ]
+
+
+@api.get(
+    "/api/network/{network_name}/",
+    response_model=FastUI,
+    response_model_exclude_none=True,
+)
+async def social_account(network_name: str) -> list[AnyComponent]:
+    """
+    User social account page, the frontend will fetch this when the user visits `/network/{name}/`.
+    """
+    social_accounts = await SocialAccount.find(
+        SocialAccount.network.name == network_name, fetch_links=True
+    ).to_list()
+    return [
+        c.Page(
+            components=[
+                c.Heading(text=network_name, level=2),
+                c.Link(components=[c.Text(text="Back")], on_click=BackEvent()),
+                c.Table(
+                    data=social_accounts,
+                    columns=[
+                        DisplayLookup(
+                            field="username",
+                        ),
+                        DisplayLookup(field="user", mode=DisplayMode.json),
+                    ],
+                ),
+            ]
+        ),
+    ]
+
+
+@api.get("/{path:path}")
+async def homepage() -> HTMLResponse:
+    return HTMLResponse(prebuilt_html(title="People"))
